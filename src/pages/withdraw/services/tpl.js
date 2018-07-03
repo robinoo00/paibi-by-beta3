5ceefcfc19151fcc091005d1e0a2b84f74bd84ba
bloackoo00 <460544293@qq.com>
2018-07-03 08:53:06 +0800

    优化

--- src/pages/withdraw/services/tpl.js
+++ src/pages/withdraw/services/tpl.js
@@ -2,7 +2,7 @@ import request from '../../../utils/request'
 import config from '../../../utils/config'
 
 export function withdraw(values){
-    return request(config.server + 'appapi/chujinact',{
+    return request(config.server + 'chujin',{
         method:'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
